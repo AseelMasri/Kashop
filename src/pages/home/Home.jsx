aefde0737fb182bae5341eb681d4bad5e2efc9bd
AseelMasri <masriaseel70@gmail.com>
2025-10-29 22:43:20 +0200

    Fix import path for Categories component (case sensitivity)

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,7 @@
 //import { Typography } from "@mui/material";
 
 import Brands from "../../components/brands/Brands";
-import Categories from "../../components/Categories/Categories";
+import Categories from "../../components/categories/Categories";
 import Products from "../../components/products/Products";
 
 
